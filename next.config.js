a9aa1fc2017bcd9d9a7199ac8d90c7b355456790
4richis <pallets-craws.0q@icloud.com>
2024-10-23 22:20:32 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  distDir: 'out',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
+  assetPrefix: './',
 }
 
 module.exports = nextConfig
